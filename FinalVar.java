f346742def1e32f19a06fbc7a4d543646afe196d
Sandhya <sandhyauppen37@gmail.com>
2026-03-13 10:11:57 +0530

    Create FinalVar.java to calculate circle area
    Added a class to calculate the area of a circle using a constant for PI.

---
+++ FinalVar.java
@@ -0,0 +1,17 @@
+public class FinalVar {
+
+public static void main(String[] args) {
+
+// Declare a constant PI
+
+final double PI = 3.14;
+
+double radius = 5;
+
+double area PI radius radius;
+
+System.out.println("Radius: " + radius);
+
+System.out.println("Area of circle: " + area);
+
+// PI = 3.14159; }
